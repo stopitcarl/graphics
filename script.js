160f5a167b9ff275f2bb94211caf0a4b85e8cb83
João Porto <joaofporto@tecnico.ulisboa.pt>
2019-09-25 20:26:18 +0100

    Created a table template for creating new objects

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
 var camera, scene, renderer;
-var geometry, material, mesh;
+var table;
 
 init();
 animate();
-var up = 1;
+var up = true;
+//var step = 0;
+
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = 1;
-    //camera.position.y = 0.1;
-    //camera.position.x = 0.1;
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    camera.position.z = 1;    
 
     scene = new THREE.Scene();
-    
 
-	// geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	// material = new THREE.MeshNormalMaterial();
 
-    // mesh = new THREE.Mesh( geometry, material );
     table = createTable();
-    scene.add( table );
-	// scene.add( mesh );
+    scene.add(table);
+    // scene.add( mesh );
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
 }
 
 function animate() {
 
-	requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
-//	mesh.rotation.x += 0.01;
-    //mesh.rotation.y += 0.02;    
-    if(camera.position.y > 0.1)
-        up =-1;
-    else if(camera.position.y < -0.1)
-        up = 1;        
-    
-    camera.position.y += 0.01* up;
+    table.rotation.x += 0.01;
+    table.rotation.y += 0.02;
+    //    camera.rotation.x = Math.sin(step)*0.2
+    //step += 0.02;
 
-	renderer.render( scene, camera );
+
+
+
+    renderer.render(scene, camera);
 
 }
-
-
-
--- table.js
+++ table.js
@@ -1,5 +1,5 @@
 function createTable(){
-	geometry = new THREE.BoxGeometry( 0.7, 0.2, 0.05 );
+	geometry = new THREE.BoxGeometry( 0.3, 0.3, 0.3 );
     material = new THREE.MeshNormalMaterial();
     mesh = new THREE.Mesh( geometry, material );
     return mesh;
