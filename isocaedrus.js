1d4276dd32b386b3825df0bfc1354ad1540937b4
jafonsoc <jon.afonso.coutinho@gmail.com>
2019-10-31 20:23:40 +0000

    Made cube by hand

--- isocaedrus.js
+++ isocaedrus.js
@@ -1,19 +1,39 @@
 class Isocaedrus extends THREE.Mesh {
     constructor() {
-        // 5x2.5 car with 0.5 radius wheels
-        let geometry = new THREE.SphereBufferGeometry(1, 5, 3);
+        // geometry
+        let geometry = new THREE.Geometry();
+
+        // vertices
+        geometry.vertices = [
+            new THREE.Vector3(-1, -1, -1), new THREE.Vector3(1, -1, -1),
+            new THREE.Vector3(-1, 1, -1), new THREE.Vector3(1, 1, -1),
+            new THREE.Vector3(-1, -1, 1), new THREE.Vector3(1, -1, 1),
+            new THREE.Vector3(-1, 1, 1), new THREE.Vector3(1, 1, 1)
+        ];
+
+        // faces
+        geometry.faces.push(
+            new THREE.Face3(0, 1, 3), new THREE.Face3(3, 2, 0),
+            new THREE.Face3(7, 5, 4), new THREE.Face3(4, 6, 7),
+            new THREE.Face3(4, 0, 2), new THREE.Face3(2, 6, 4),
+            new THREE.Face3(1, 5, 7), new THREE.Face3(7, 3, 1),
+            new THREE.Face3(1, 0, 4), new THREE.Face3(4, 5, 1),
+            new THREE.Face3(6, 2, 3), new THREE.Face3(3, 7, 6)
+        );
+
+
+        // normals
+        geometry.computeFaceNormals();
+        geometry.computeVertexNormals();
+
         let material = new THREE.MeshBasicMaterial({
-            //        flatShading: true,
             color: 0x4083c7,
             wireframe: !isWireframe
         });
         super(geometry, material);
-        this.position.y = 1;
-
-        var axesHelper = new THREE.AxesHelper(1);
-        this.add(axesHelper);
-        this.axisHelp = axesHelper;
-        this.axisHelp.visible = ballAxisVisible;
+        this.position.x = 1;
+        this.position.y = 3;
+        this.position.z = 1;
     }
 
     toggleAxes(bool) {
